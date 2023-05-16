cb9a7399207b10729ccc546f79f3c712bfe4b374
Aslan <korpeevaslan@gmail.com>
2023-05-16 06:24:39 +0600

    feat(config): add axios response interceptor auth axios instance

--- shared/config/api.ts
+++ shared/config/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 import { RuntimeConfig } from "nuxt/schema";
 
 const baseApiAbortController = newAbortSignal();
@@ -8,7 +8,10 @@ function newAbortSignal() {
   return new AbortController();
 }
 
-const getAxiosInstances = (config: RuntimeConfig) => {
+const getAxiosInstances = (
+  config: RuntimeConfig,
+  cb?: (...args: any[]) => void
+) => {
   const baseApiInstance = axios.create({
     baseURL: config.public.apiURL,
     signal: baseApiAbortController.signal,
@@ -18,10 +21,24 @@ const getAxiosInstances = (config: RuntimeConfig) => {
     baseURL: config.public.apiURL,
     signal: authApiAbortController.signal,
   });
+  authApiInstance.interceptors.response.use(
+    (config) => config,
+    axiosResponseRejectInterceptor(authApiInstance, cb)
+  );
   return {
     authApiInstance,
     baseApiInstance,
   };
 };
-
+const axiosResponseRejectInterceptor =
+  (axiosInstance: AxiosInstance, cb?: (...args: any[]) => void) =>
+  (error: any) => {
+    const originalConfig = error.config;
+    if (error.response.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      cb?.();
+      return axiosInstance(originalConfig);
+    }
+    return Promise.reject(error);
+  };
 export { authApiAbortController, baseApiAbortController, getAxiosInstances };
